12c042ae5d7479663c35b50cc24371db31085a37
Don Li <zbdiablo@gmail.com>
2011-03-14 21:56:36 +0800

    mapred word count sample

---
+++ src/main/java/com/jardon/pg/cluster/hadoop/wordcount/mapper/WordCountMapper.java
@@ -0,0 +1,25 @@
+package com.jardon.pg.cluster.hadoop.wordcount.mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    
+    private final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer tokenizer = new StringTokenizer(value.toString());
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            context.write(word, one);
+        }
+    }
+    
+}
